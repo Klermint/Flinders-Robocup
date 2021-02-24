7a9f6a7cf379c62fa8686710fcbc9d13c8d9635b
Klermint <74746572+Klermint@users.noreply.github.com>
2021-02-24 14:41:57 +1030

    Add files via upload

---
+++ Image-Processing/3. Hough Transform/main.py
@@ -0,0 +1,48 @@
+"""
+This program is used to apply Hough Transform on various images
+to fine-tune the parameters of the Hough Circle Function.
+Best parameters for test_gray: minDist=1000, param1=1, param2=1
+Best parameters for red3: minDist=1000, param1=1, param2=1
+Best parameter for red3: minDist=1, param1=50, param2=18
+"""
+
+# import the necessary packages
+import cv2
+import numpy as np
+
+# define the lower and upper boundaries of the "red"
+lower = (0, 100, 80)
+upper = (10, 255, 255)
+
+# read the file
+original_img = cv2.imread('red3.png')
+cv2.imshow('Inputted Image', original_img)
+
+# blur and convert to HSV
+blur = cv2.GaussianBlur(original_img, (11, 11), 0)
+gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+cv2.imshow('gray', gray)
+hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+
+# construct a mask for colour, perform erosion & dilation
+mask = cv2.inRange(hsv, lower, upper)
+mask = cv2.erode(mask, None, iterations=2)
+mask = cv2.dilate(mask, None, iterations=2)
+cv2.imshow('mask', mask)
+
+# Hough Transform
+circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp= 1, minDist=1000,
+                           param1=1, param2=1, minRadius=0, maxRadius=0)
+
+circles = np.uint16(np.around(circles)) # error occurs here
+
+for i in circles[0,:]:
+    # draw the outer circle
+    cv2.circle(original_img,(i[0],i[1]),i[2],(0,255,0),2)
+    # draw the center of the circle
+    cv2.circle(original_img,(i[0],i[1]),2,(255,255,255),3)
+
+cv2.imshow('Hough Transform', original_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
