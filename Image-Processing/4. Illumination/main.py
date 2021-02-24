4b07edb7a2cb42407abc429c1850d792bff9e307
Klermint <74746572+Klermint@users.noreply.github.com>
2021-02-24 14:57:42 +1030

    Add files via upload

--- Image-Processing/4. Illumination/main.py
+++ Image-Processing/4. Illumination/main.py
@@ -1,105 +1,75 @@
 # import the necessary packages
+from collections import deque
+from imutils.video import VideoStream
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+import imutils
+import argparse
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
+args = vars(ap.parse_args())
 
 # define the lower and upper boundaries of the "red"
 lower = (0, 100, 80)
 upper = (10, 255, 255)
+pts = deque(maxlen=args["buffer"])
 
-# read the file
-original_img = cv2.imread('red3.png')
-cv2.imshow('original_img', original_img)
+# get webcam
+camera = VideoStream(src=0).start()
 
-# HISTOGRAM EQUALISATION
-"""
-# HSV
-hsv = cv2.cvtColor(original_img, cv2.COLOR_BGR2HSV)
-cv2.imshow('HSV', hsv)
+# keep looping
+while True:
+    # get the current frame
+    frame = camera.read()
+    cv2.imshow('original frame', frame)
+    # convert to HSV
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    cv2.imshow('hsv', hsv)
+    # split HSV
+    h, s, v = cv2.split(hsv)
+    cv2.imshow('v', v)
+    # histogram equalize on value range
+    v_hist = cv2.equalizeHist(v)
+    cv2.imshow('v_hist', v_hist)
+    # merge back to hsv
+    hsv_merge = cv2.merge([h, s, v_hist])
+    cv2.imshow('hsv_merge', hsv_merge)
+    bgr = cv2.cvtColor(hsv_merge, cv2.COLOR_HSV2BGR)
+    cv2.imshow('bgr', bgr)
+    # construct a mask for colour, perform erosion & dilation
+    mask = cv2.inRange(hsv_merge, lower, upper)
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+    cv2.imshow('mask', mask)
+    # perform Hough Transform
+    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp=1, minDist=1000,
+                               param1=50, param2=15, minRadius=0, maxRadius=0)
+    # original parameters: (minDist=1000,param1=50, param2=15, minRadius=0, maxRadius=0)
+    # proceed if there is a circle
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        # draw circles
+        for i in circles[0, :]:
+            # draw the outer circle
+            cv2.circle(bgr, (i[0], i[1]), i[2], (255, 255, 255), 2)
+            # draw the center of the circle
+            cv2. circle(bgr, (i[0], i[1]), 2, (255, 255, 255), 3)
+        center = (i[0], i[1])
+        # update the points queue
+        pts.appendleft(center)
 
-# Split HSV
-h, s, v1 = cv2.split(hsv)
-# cv2.imshow('h', h)
-# cv2.imshow('s', s)
-cv2.imshow("v1",v1)
+        # loop over the set of tracked points
+        for i in range(1, len(pts)):
+            # if either of the tracked points are None, ignore them
+            if pts[i - 1] is None or pts[i] is None:
+                continue
 
-# V equalize
-v_hist = cv2.equalizeHist(v1)
-cv2.imshow('v_hist', v_hist)
+            # otherwise, compute the thickness of the line and draw connecting lines
+            # thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            # cv2.line(frame, pts[i - 1], pts[i], (255, 255, 255), thickness)
+    cv2.imshow('Hough Transform', bgr)
+    key = cv2.waitKey(1) & 0xFF
 
-# merge hsv back and convert the bgr
-hsv = cv2.merge([h, s, v_hist])
-res = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
-cv2.imshow('bgr', res)
-"""
-
-# GAMMA correction
-# """
-lookUpTable = np.empty((1, 256), np.uint8)
-for i in range(256):
-    lookUpTable[0, i] = np.clip(pow(i / 255.0, 0.6) * 255.0, 0, 255)
-res = cv2.LUT(original_img, lookUpTable)
-cv2.imshow('gamma', res)
-
-hsv_merge = cv2.cvtColor(res, cv2.COLOR_BGR2HSV)
-# """
-
-# ADAPTIVE FILTER
-"""
-# HSV
-HSV = cv2.cvtColor(original_img, cv2.COLOR_BGR2HSV)
-cv2.imshow('HSV', HSV)
-
-# HSV2GRAY
-h, s, v1 = cv2.split(HSV)
-cv2.imshow('h', h)
-cv2.imshow('s', s)
-cv2.imshow("v1",v1)
-
-# Traditional Thresholding
-ret,th1 = cv2.threshold(GRAY,127,255,cv2.THRESH_BINARY)
-cv2.imshow('globalThresholding', th1)
-
-# Adaptive Mean Thresholding
-th2 = cv2.adaptiveThreshold(GRAY,255,cv2.ADAPTIVE_THRESH_MEAN_C,
-                                cv2.THRESH_BINARY,11,2)
-cv2.imshow('adaptiveMeanThresholding', th2)
-
-# Adaptive Gaussian Thresholding
-th3 = cv2.adaptiveThreshold(GRAY, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                                cv2.THRESH_BINARY, 11, 2)
-cv2.imshow('adaptiveGaussianThreshold', th3)
-
-# Gaussian Blur + Otsu Thresholding
-blur = cv2.GaussianBlur(GRAY, (11, 11), 0)
-ret4,th4 = cv2.threshold(blur,255,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-cv2.imshow('Otsu', th4)
-
-# Gaussian Blur + Otsu_HSV Thresholding
-blur = cv2.GaussianBlur(v1, (11, 11), 0)
-ret4,th4 = cv2.threshold(blur,255,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
-cv2.imshow('Otsu_HSV', th4)
-"""
-
-# construct a mask for colour, perform erosion & dilation
-mask = cv2.inRange(hsv_merge, lower, upper)
-mask = cv2.erode(mask, None, iterations=2)
-mask = cv2.dilate(mask, None, iterations=2)
-cv2.imshow('mask', mask)
-
-# perform Hough Transform
-circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp=1, minDist=1000,
-                           param1=1, param2=1, minRadius=0, maxRadius=0)
-# param1=50, param2=30
-# draw circles
-for i in circles[0, :]:
-    cv2.circle(res, (i[0], i[1]), i[2], (255, 255, 255), 2)
-
-cv2.imshow('hough', res)
-
-# display histogram
-# plt.hist(res.ravel(), 256, [0,256])
-# plt.show()
-
-cv2.waitKey(0)
 cv2.destroyAllWindows()
