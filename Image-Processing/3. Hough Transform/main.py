7a6176cea577e9e37e70f621c7066b0606a3a0f1
Klermint <74746572+Klermint@users.noreply.github.com>
2021-02-25 12:20:41 +1030

    Update main.py

--- Image-Processing/3. Hough Transform/main.py
+++ Image-Processing/3. Hough Transform/main.py
@@ -2,8 +2,8 @@
 This program is used to apply Hough Transform on various images
 to fine-tune the parameters of the Hough Circle Function.
 Best parameters for test_gray: minDist=1000, param1=1, param2=1
-Best parameters for red3: minDist=1000, param1=1, param2=1
 Best parameter for red3: minDist=1, param1=50, param2=18
+Best parameters to fit any image: minDist=1000, param1=1, param2=1 (accuracy decreases)
 """
 
 # import the necessary packages
@@ -20,8 +20,6 @@ cv2.imshow('Inputted Image', original_img)
 
 # blur and convert to HSV
 blur = cv2.GaussianBlur(original_img, (11, 11), 0)
-gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
-cv2.imshow('gray', gray)
 hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
 # construct a mask for colour, perform erosion & dilation
@@ -30,18 +28,18 @@ mask = cv2.erode(mask, None, iterations=2)
 mask = cv2.dilate(mask, None, iterations=2)
 cv2.imshow('mask', mask)
 
-# Hough Transform
+# Perform Hough Transform
 circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp= 1, minDist=1000,
                            param1=1, param2=1, minRadius=0, maxRadius=0)
 
-circles = np.uint16(np.around(circles)) # error occurs here
-
+# Draw a circle for each circle detected by Hough Transform
 for i in circles[0,:]:
     # draw the outer circle
     cv2.circle(original_img,(i[0],i[1]),i[2],(0,255,0),2)
     # draw the center of the circle
     cv2.circle(original_img,(i[0],i[1]),2,(255,255,255),3)
 
+# Display Circle Found
 cv2.imshow('Hough Transform', original_img)
 
 cv2.waitKey(0)
